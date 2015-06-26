0b08ed999c7d42a34cff33ccd3da51fdddf1d6fe
niannian199 <niannian199@sina.com>
2015-06-26 16:19:49 +0800

    'init'

---
+++ src/main/java/com/jyn/jar_demo/App.java
@@ -0,0 +1,13 @@
+package com.jyn.jar_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
