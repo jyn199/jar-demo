1168c2b24cd6dad4351bfc386595d4edf9c26338
niannian199 <niannian199@sina.com>
2015-06-26 17:16:51 +0800

    'add_jar-with-dependencies'

--- src/main/java/com/jyn/jar_demo/App.java
+++ src/main/java/com/jyn/jar_demo/App.java
@@ -1,13 +1,24 @@
 package com.jyn.jar_demo;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ClientProtocolException, IOException
     {
         System.out.println( "Hello World!" );
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        client.execute(get);
+        client.close();
     }
 }
